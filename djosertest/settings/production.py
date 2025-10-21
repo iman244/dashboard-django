1cbd103836666f38799959d8cce593fbb36b0719
iman <imansharifi244@gmail.com>
2025-10-21 13:03:23 +0330

    ul

--- djosertest/settings/production.py
+++ djosertest/settings/production.py
@@ -3,7 +3,7 @@ from .base import *
 ALLOWED_HOSTS = ['*']
 
 CORS_ALLOWED_ORIGINS = [
-    "https://medicdashboard-nextjs.liara.run",
+    "https://medicdashboard-nextjs.golden-horde.ir",
 ]
 
 DATABASES = {
